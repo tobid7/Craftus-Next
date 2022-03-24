ba57a9a1c933a9da2e5c5f47b777e8f8ee17c082
skymz4 <99190689+skymz4@users.noreply.github.com>
2022-03-24 18:52:18 +0100

    Fix the trees

--- source/world/worldgen/structures/TreeGen.c
+++ source/world/worldgen/structures/TreeGen.c
@@ -3,46 +3,42 @@
 
 void TreeGen_GenTree(WorkQueue* queue, WorkerItem item,int x, int y, int z, int height)
 {
-    for (int hx = y + height - 4; hx < y + height; hx++){
-        int g1 = y + height - 4;
-        int g2 = y + height - 3;
-        int g3 = y + height - 2;
-        int g4 = y + height - 1;
+    for (int hx = 0; hx < 4; hx++){
         switch(hx)
         {
-            case g1:
+            case 0:
                 for (int l = z - 2; l < z + 3; l++)
                 {
                     for (int lx = x - 2;  lx < x + 3; lx ++)
                     {
-                        Chunk_SetBlock(item.chunk, lx, hx, l, Block_Leaves);
+                        Chunk_SetBlock(item.chunk, lx, y + height - 4, l, Block_Leaves);
                     }
                 }
                 break;
-            case g2:
+            case 1:
                 for (int l = z - 2; l < z + 3; l++)
                 {
                     for (int lx = x - 2;  lx < x + 3; lx ++)
                     {
-                        Chunk_SetBlock(item.chunk, lx, hx, l, Block_Leaves);
+                        Chunk_SetBlock(item.chunk, lx, y + height - 3, l, Block_Leaves);
                     }
                 }
                 break;
-            case g3:
+            case 2:
                 for (int l = z - 1; l < z + 2; l++)
                 {
                     for (int lx = x - 1;  lx < x + 2; lx ++)
                     {
-                        Chunk_SetBlock(item.chunk, lx, hx, l, Block_Leaves);
+                        Chunk_SetBlock(item.chunk, lx, y + height - 2, l, Block_Leaves);
                     }
                 }
                 break;
-            case g4:
+            case 3:
                 for (int l = z - 0; l < z + 1; l++)
                 {
                     for (int lx = x - 0;  lx < x + 1; lx ++)
                     {
-                        Chunk_SetBlock(item.chunk, lx, hx, l, Block_Leaves);
+                        Chunk_SetBlock(item.chunk, lx, y + height - 1, l, Block_Leaves);
                     }
                 }
                 break;
