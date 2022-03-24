eda945dc5e668d83164df0145d44363244f0bbb8
skymz4 <99190689+skymz4@users.noreply.github.com>
2022-03-24 17:12:24 +0100

    render debug on topscreen

--- source/rendering/Renderer.c
+++ source/rendering/Renderer.c
@@ -175,7 +175,7 @@ void Renderer_Render() {
 
 		C3D_BindProgram(&gui_shader);
 		C3D_SetAttrInfo(&gui_vertexAttribs);
-
+                if (showDebugInfo) DebugUI_Draw();
 		SpriteBatch_Render(GFX_TOP);
 
 		if (iod <= 0.f) break;
@@ -198,7 +198,7 @@ void Renderer_Render() {
 					  &player->quickSelectBarSlot);
 		player->inventorySite = Inventory_Draw(16, 0, 160, player->inventory, sizeof(player->inventory) / sizeof(ItemStack),player->inventorySite);
 
-		if (showDebugInfo) DebugUI_Draw();
+		
 	}
 
 	Gui_Frame();
