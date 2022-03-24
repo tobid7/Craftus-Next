ebcad1269551117338d3b078179e44ba49dc9ddf
skymz4 <99190689+skymz4@users.noreply.github.com>
2022-03-24 21:30:10 +0100

    Update SuperFlatGen.c

--- source/world/worldgen/SuperFlatGen.c
+++ source/world/worldgen/SuperFlatGen.c
@@ -26,16 +26,14 @@ void SuperFlatGen_Generate(WorkQueue* queue, WorkerItem item, void* this) {
 				block = Block_Air;
 				break;
 		}
-        
+                TreeGen_GenTree(queue, item, 2, 17, 6, 6);
 		for (int x = 0; x < CHUNK_SIZE; x++)
 			for (int z = 0; z < CHUNK_SIZE; z++) 
                         {Chunk_SetBlock(item.chunk, x, y, z, block);
-                         int jh = rand() % 200;
-                         if (jh == 1)
-		         {
-			       TreeGen_GenTree(queue, item, x, 17, z, 6);
+                        
+			       
 			
-		         }
+		         
                  }
 	}
 }
