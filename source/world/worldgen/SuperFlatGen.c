6216268f3c614d6b234ceffa9ac03688c58310ff
skymz4 <99190689+skymz4@users.noreply.github.com>
2022-03-24 10:56:48 +0100

    add trees on flat

--- source/world/worldgen/SuperFlatGen.c
+++ source/world/worldgen/SuperFlatGen.c
@@ -6,7 +6,7 @@
 void SuperFlatGen_Init(SuperFlatGen* gen, World* world) {}
 
 void SuperFlatGen_Generate(WorkQueue* queue, WorkerItem item, void* this) {
-	for (int y = 0; y < 17; y++) {
+	for (int y = 0; y < 20; y++) {
 		Block block = Block_Air;
 		switch (y) {  // TODO: Mit einem Lookup Table ersetzen, Superflach Gen konfigurierbar machen
 			case 0:
@@ -25,7 +25,12 @@ void SuperFlatGen_Generate(WorkQueue* queue, WorkerItem item, void* this) {
 				block = Block_Air;
 				break;
 		}
-                
+                for (int i = 2; i < 3; i++)
+			{
+				int treex = 5;
+				int treez = 10;
+				TreeGen_GenTree(queue, item, treex, 17, treez, 3);
+			}
 		for (int x = 0; x < CHUNK_SIZE; x++)
 			for (int z = 0; z < CHUNK_SIZE; z++) Chunk_SetBlock(item.chunk, x, y, z, block);
 	}
