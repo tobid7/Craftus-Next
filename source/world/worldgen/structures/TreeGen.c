0168643fcf6d031ab92e121b0753d11bf0ac2248
skymz4 <99190689+skymz4@users.noreply.github.com>
2022-03-24 16:24:12 +0100

    Update TreeGen.c

--- source/world/worldgen/structures/TreeGen.c
+++ source/world/worldgen/structures/TreeGen.c
@@ -6,7 +6,7 @@ void TreeGen_GenTree(WorkQueue* queue, WorkerItem item,int x, int y, int z, int 
     for (int hx = y + height - 4; hx < y + height; hx++){
         for (int l = z - 2; l < z + 3; l++)
         {
-            for (int lx = x - 3;  lx < x + 2; lx ++)
+            for (int lx = x - 2;  lx < x + 3; lx ++)
             {
                   Chunk_SetBlock(item.chunk, lx, hx, l, Block_Leaves);
             }
