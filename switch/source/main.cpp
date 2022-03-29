55b276b15cb4737b025b6026a46d3410b2e2349a
Tobi <tobid7@outlook.de>
2022-03-29 16:44:46 +0200

    Add Input Exit Func

--- switch/source/main.cpp
+++ switch/source/main.cpp
@@ -4,10 +4,16 @@ NGS::Game game("Craftus-Next");
 
 int main()
 {
+    padConfigureInput(1, HidNpadStyleSet_NpadStandard);
+    PadState pad;
+    padInitializeDefault(&pad);
 
     while (game.IsRunning())
     {
-
+        padUpdate(&pad);
+        u32 kDown = padGetButtonsDown(&pad);
+        if (kDown & HidNpadButton_Plus)
+            game.Exit();
     }
 
     return 0;
