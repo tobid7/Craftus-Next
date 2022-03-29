498aa4dff067384b8bb6d46124527d30286a4bf2
Tobi <tobid7@outlook.de>
2022-03-29 16:54:45 +0200

    _______

---
+++ switch/source/NPINX/shaders/2D.h
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static const char* const _2Dvs = R"text(
+    #version 330 core
+    layout (location = 0) in vec3 aPos;
+    layout (location = 1) in vec3 aColor;
+    out vec3 ourColor;
+    void main()
+    {
+        gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+        ourColor = aColor;s
+    }
+)text";
+
+static const char* const _2Dfs = R"text(
+    #version 330 core
+    in vec3 ourColor;
+    out vec4 fragColor;
+    void main()
+    {
+        fragColor = vec4(ourColor, 1.0f);
+    }
+)text";
