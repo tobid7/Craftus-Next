bf904fe3dbff1c2edca5e8e3fa360608cf954a52
skymz4 <99190689+skymz4@users.noreply.github.com>
2022-03-31 16:51:33 +0200

    Acacia

--- 3ds/source/world/worldgen/structures/AcaciaTree.c
+++ 3ds/source/world/worldgen/structures/AcaciaTree.c
@@ -6,31 +6,31 @@ void AcaciaTree_Gen(WorkQueue* queue, WorkerItem item,int x, int y, int z, int h
         switch(hx)
         {
             case 0:
-                for (int l = z - 4; l < z + 5; l++)
+                for (int l = z - 3; l < z + 4; l++)
                 {
-                    for (int lx = x - 4;  lx < x + 5; lx ++)
+                    for (int lx = x - 3;  lx < x + 4; lx ++)
                     {
                         Chunk_SetBlock(item.chunk, lx, y + height - 2, l, Block_LeavesAcacia);
                     }
                 }
                 break;
             case 1:
-                for (int l = z - 2; l < z + 3; l++)
+                for (int l = z - 1; l < z + 2; l++)
                 {
-                    for (int lx = x - 2;  lx < x + 3; lx ++)
+                    for (int lx = x - 1;  lx < x + 2; lx ++)
                     {
                         Chunk_SetBlock(item.chunk, lx, y + height - 1, l, Block_LeavesAcacia);
-                        Chunk_SetBlock(item.chunk, lx-3, y + height - 3, l, Block_LeavesAcacia);
-                        Chunk_SetBlock(item.chunk, lx+3, y + height - 3, l, Block_LeavesAcacia);
-                        Chunk_SetBlock(item.chunk, lx, y + height - 1, l+4, Block_LeavesAcacia);
-                        Chunk_SetBlock(item.chunk, lx, y + height - 1, l-4, Block_LeavesAcacia);
+                        
                     }
                 }
                 break;
             default: break;
         }
     }
-    
+    Chunk_SetBlock(item.chunk, x-3, y + height - 1, z, Block_LeavesAcacia);
+    Chunk_SetBlock(item.chunk, x+3, y + height - 1, z, Block_LeavesAcacia);
+    Chunk_SetBlock(item.chunk, x, y + height - 1, z+3, Block_LeavesAcacia);
+    Chunk_SetBlock(item.chunk, x, y + height - 1, z-3, Block_LeavesAcacia);
     for (int i = y; i < y + height-1; i++)
     {
         Chunk_SetBlock(item.chunk, x, i, z, Block_LogAcacia);
