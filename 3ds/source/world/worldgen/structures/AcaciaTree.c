a0fb2caa97559e7c79fc20dfc723a43526d81ab4
skymz4 <99190689+skymz4@users.noreply.github.com>
2022-03-31 16:45:30 +0200

    Acacia Trees...

--- 3ds/source/world/worldgen/structures/AcaciaTree.c
+++ 3ds/source/world/worldgen/structures/AcaciaTree.c
@@ -6,22 +6,22 @@ void AcaciaTree_Gen(WorkQueue* queue, WorkerItem item,int x, int y, int z, int h
         switch(hx)
         {
             case 0:
-                for (int l = z - 4; l < z + 4; l++)
+                for (int l = z - 4; l < z + 5; l++)
                 {
-                    for (int lx = x - 4;  lx < x + 4; lx ++)
+                    for (int lx = x - 4;  lx < x + 5; lx ++)
                     {
                         Chunk_SetBlock(item.chunk, lx, y + height - 2, l, Block_LeavesAcacia);
                     }
                 }
                 break;
             case 1:
-                for (int l = z - 3; l < z + 3; l++)
+                for (int l = z - 2; l < z + 3; l++)
                 {
-                    for (int lx = x - 3;  lx < x + 3; lx ++)
+                    for (int lx = x - 2;  lx < x + 3; lx ++)
                     {
                         Chunk_SetBlock(item.chunk, lx, y + height - 1, l, Block_LeavesAcacia);
-                        Chunk_SetBlock(item.chunk, lx-4, y + height - 4, l, Block_LeavesAcacia);
-                        Chunk_SetBlock(item.chunk, lx+4, y + height - 4, l, Block_LeavesAcacia);
+                        Chunk_SetBlock(item.chunk, lx-3, y + height - 3, l, Block_LeavesAcacia);
+                        Chunk_SetBlock(item.chunk, lx+3, y + height - 3, l, Block_LeavesAcacia);
                         Chunk_SetBlock(item.chunk, lx, y + height - 1, l+4, Block_LeavesAcacia);
                         Chunk_SetBlock(item.chunk, lx, y + height - 1, l-4, Block_LeavesAcacia);
                     }
