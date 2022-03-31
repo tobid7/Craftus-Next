c2b3affc61803057f0d1aa8e71b07b3696ad6825
skymz4 <99190689+skymz4@users.noreply.github.com>
2022-03-31 16:35:14 +0200

    Generate 100% Savanne because of tree debugging xd

--- 3ds/source/world/worldgen/structures/AcaciaTree.c
+++ 3ds/source/world/worldgen/structures/AcaciaTree.c
@@ -2,46 +2,28 @@
 
 void AcaciaTree_Gen(WorkQueue* queue, WorkerItem item,int x, int y, int z, int height)
 {
-    for (int hx = 0; hx < 4; hx++){
+    for (int hx = 0; hx < 2; hx++){
         switch(hx)
         {
             case 0:
-                for (int l = z - 2; l < z + 3; l++)
+                for (int l = z - 4; l < z + 4; l++)
                 {
-                    for (int lx = x - 2;  lx < x + 3; lx ++)
-                    {
-                        Chunk_SetBlock(item.chunk, lx, y + height - 4, l, Block_LeavesAcacia);
-                    }
-                }
-                break;
-            case 1:
-                for (int l = z - 2; l < z + 3; l++)
-                {
-                    for (int lx = x - 2;  lx < x + 3; lx ++)
-                    {
-                        Chunk_SetBlock(item.chunk, lx, y + height - 3, l, Block_LeavesAcacia);
-                    }
-                }
-                break;
-            case 2:
-                for (int l = z - 1; l < z + 2; l++)
-                {
-                    for (int lx = x - 1;  lx < x + 2; lx ++)
+                    for (int lx = x - 4;  lx < x + 4; lx ++)
                     {
                         Chunk_SetBlock(item.chunk, lx, y + height - 2, l, Block_LeavesAcacia);
                     }
                 }
                 break;
-            case 3:
-                for (int l = z - 0; l < z + 1; l++)
+            case 1:
+                for (int l = z - 3; l < z + 3; l++)
                 {
-                    for (int lx = x - 0;  lx < x + 1; lx ++)
+                    for (int lx = x - 3;  lx < x + 3; lx ++)
                     {
                         Chunk_SetBlock(item.chunk, lx, y + height - 1, l, Block_LeavesAcacia);
-                        Chunk_SetBlock(item.chunk, lx-1, y + height - 1, l, Block_LeavesAcacia);
-                        Chunk_SetBlock(item.chunk, lx+1, y + height - 1, l, Block_LeavesAcacia);
-                        Chunk_SetBlock(item.chunk, lx, y + height - 1, l+1, Block_LeavesAcacia);
-                        Chunk_SetBlock(item.chunk, lx, y + height - 1, l-1, Block_LeavesAcacia);
+                        Chunk_SetBlock(item.chunk, lx-4, y + height - 4, l, Block_LeavesAcacia);
+                        Chunk_SetBlock(item.chunk, lx+4, y + height - 4, l, Block_LeavesAcacia);
+                        Chunk_SetBlock(item.chunk, lx, y + height - 1, l+4, Block_LeavesAcacia);
+                        Chunk_SetBlock(item.chunk, lx, y + height - 1, l-4, Block_LeavesAcacia);
                     }
                 }
                 break;
