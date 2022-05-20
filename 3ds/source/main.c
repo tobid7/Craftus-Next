883c08214993ab204277eee081af61a1f6684e16
Tobi <tobid7@outlook.de>
2022-05-20 10:08:42 +0200

    Output in mb

--- 3ds/source/main.c
+++ 3ds/source/main.c
@@ -113,8 +113,8 @@ int main() {
 			//changeFile("romfs:/assets/sound/music/Minecraft.ogg", &soundg);
 		}
 		
-		DebugUI_Text("%d FPS  Usage: CPU: %5.2f%% GPU: %5.2f%% Buf: %5.2f%% Lin: %d", fps, C3D_GetProcessingTime() * 6.f,
-		C3D_GetDrawingTime() * 6.f, C3D_GetCmdBufUsage() * 100.f, linearSpaceFree());
+		DebugUI_Text("%d FPS  Usage: CPU: %5.2f%% GPU: %5.2f%% Buf: %5.2f%% Lin: %dmb", fps, C3D_GetProcessingTime() * 6.f,
+		C3D_GetDrawingTime() * 6.f, C3D_GetCmdBufUsage() * 100.f, linearSpaceFree() / 1024 / 1024);
 		DebugUI_Text("X: %f, Y: %f, Z: %f", f3_unpack(player.position));
 		//DebugUI_Text("HP: %i",player.hp);
 		//DebugUI_Text("velocity: %f rndy: %f",player.velocity.y,player.rndy);
