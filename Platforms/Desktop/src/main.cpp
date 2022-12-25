dc9e2424aaf3194f8ff4eb74643a7f7d2e144c2b
Tobi-D7 <tobid7@outlook.de>
2022-12-26 00:38:54 +0100

    Some BugFix

--- Platforms/Desktop/src/main.cpp
+++ Platforms/Desktop/src/main.cpp
@@ -27,18 +27,23 @@
 
 unsigned long hex2dec(std::string hex)
 {
-    unsigned long result = 0;
-    for (int i=0; i<hex.length(); i++) {
-        if (hex[i]>=48 && hex[i]<=57)
-        {
-            result += (hex[i]-48)*pow(16,hex.length()-i-1);
-        } else if (hex[i]>=65 && hex[i]<=70) {
-            result += (hex[i]-55)*pow(16,hex.length( )-i-1);
-        } else if (hex[i]>=97 && hex[i]<=102) {
-            result += (hex[i]-87)*pow(16,hex.length()-i-1);
-        }
+  unsigned long result = 0;
+  for (int i = 0; i < hex.length(); i++)
+  {
+    if (hex[i] >= 48 && hex[i] <= 57)
+    {
+      result += (hex[i] - 48) * pow(16, hex.length() - i - 1);
     }
-    return result;
+    else if (hex[i] >= 65 && hex[i] <= 70)
+    {
+      result += (hex[i] - 55) * pow(16, hex.length() - i - 1);
+    }
+    else if (hex[i] >= 97 && hex[i] <= 102)
+    {
+      result += (hex[i] - 87) * pow(16, hex.length() - i - 1);
+    }
+  }
+  return result;
 }
 
 int CalcPMT(int numattr)
@@ -50,7 +55,7 @@ int CalcPMT(int numattr)
     hexstr += std::to_string(i);
   }
   res = hex2dec(hexstr);
-  
+
   return res;
 }
 
@@ -59,104 +64,109 @@ std::unique_ptr<Base::StealConsole> stolenc;
 
 std::string task_ = "";
 
-std::string RemoveExtPath(std::string name) {
+std::string RemoveExtPath(std::string name)
+{
   return remove_ext(GetFileName(name));
 }
 
-namespace ImGui {
+namespace ImGui
+{
 
-bool BufferingBar(const char *label, float value, const ImVec2 &size_arg,
-                  const ImU32 &bg_col, const ImU32 &fg_col) {
-  ImGuiWindow *window = GetCurrentWindow();
-  if (window->SkipItems)
-    return false;
+  bool BufferingBar(const char *label, float value, const ImVec2 &size_arg,
+                    const ImU32 &bg_col, const ImU32 &fg_col)
+  {
+    ImGuiWindow *window = GetCurrentWindow();
+    if (window->SkipItems)
+      return false;
 
-  ImGuiContext &g = *GImGui;
-  const ImGuiStyle &style = g.Style;
-  const ImGuiID id = window->GetID(label);
+    ImGuiContext &g = *GImGui;
+    const ImGuiStyle &style = g.Style;
+    const ImGuiID id = window->GetID(label);
 
-  ImVec2 pos = window->DC.CursorPos;
-  ImVec2 size = size_arg;
-  size.x -= style.FramePadding.x * 2;
+    ImVec2 pos = window->DC.CursorPos;
+    ImVec2 size = size_arg;
+    size.x -= style.FramePadding.x * 2;
 
-  const ImRect bb(pos, ImVec2(pos.x + size.x, pos.y + size.y));
-  ItemSize(bb, style.FramePadding.y);
-  if (!ItemAdd(bb, id))
-    return false;
+    const ImRect bb(pos, ImVec2(pos.x + size.x, pos.y + size.y));
+    ItemSize(bb, style.FramePadding.y);
+    if (!ItemAdd(bb, id))
+      return false;
 
-  // Render
-  const float circleStart = size.x * 0.7f;
-  const float circleEnd = size.x;
-  const float circleWidth = circleEnd - circleStart;
+    // Render
+    const float circleStart = size.x * 0.7f;
+    const float circleEnd = size.x;
+    const float circleWidth = circleEnd - circleStart;
 
-  window->DrawList->AddRectFilled(bb.Min, ImVec2(pos.x + circleStart, bb.Max.y),
-                                  bg_col);
-  window->DrawList->AddRectFilled(
-      bb.Min, ImVec2(pos.x + circleStart * value, bb.Max.y), fg_col);
+    window->DrawList->AddRectFilled(bb.Min, ImVec2(pos.x + circleStart, bb.Max.y),
+                                    bg_col);
+    window->DrawList->AddRectFilled(
+        bb.Min, ImVec2(pos.x + circleStart * value, bb.Max.y), fg_col);
 
-  const float t = g.Time;
-  const float r = size.y / 2;
-  const float speed = 1.5f;
+    const float t = g.Time;
+    const float r = size.y / 2;
+    const float speed = 1.5f;
 
-  const float a = speed * 0;
-  const float b = speed * 0.333f;
-  const float c = speed * 0.666f;
+    const float a = speed * 0;
+    const float b = speed * 0.333f;
+    const float c = speed * 0.666f;
 
-  const float o1 =
-      (circleWidth + r) * (t + a - speed * (int)((t + a) / speed)) / speed;
-  const float o2 =
-      (circleWidth + r) * (t + b - speed * (int)((t + b) / speed)) / speed;
-  const float o3 =
-      (circleWidth + r) * (t + c - speed * (int)((t + c) / speed)) / speed;
+    const float o1 =
+        (circleWidth + r) * (t + a - speed * (int)((t + a) / speed)) / speed;
+    const float o2 =
+        (circleWidth + r) * (t + b - speed * (int)((t + b) / speed)) / speed;
+    const float o3 =
+        (circleWidth + r) * (t + c - speed * (int)((t + c) / speed)) / speed;
 
-  window->DrawList->AddCircleFilled(
-      ImVec2(pos.x + circleEnd - o1, bb.Min.y + r), r, bg_col);
-  window->DrawList->AddCircleFilled(
-      ImVec2(pos.x + circleEnd - o2, bb.Min.y + r), r, bg_col);
-  window->DrawList->AddCircleFilled(
-      ImVec2(pos.x + circleEnd - o3, bb.Min.y + r), r, bg_col);
-}
-
-bool Spinner(const char *label, float radius, int thickness,
-             const ImU32 &color) {
-  ImGuiWindow *window = GetCurrentWindow();
-  if (window->SkipItems)
-    return false;
-
-  ImGuiContext &g = *GImGui;
-  const ImGuiStyle &style = g.Style;
-  const ImGuiID id = window->GetID(label);
-
-  ImVec2 pos = window->DC.CursorPos;
-  ImVec2 size((radius)*2, (radius + style.FramePadding.y) * 2);
-
-  const ImRect bb(pos, ImVec2(pos.x + size.x, pos.y + size.y));
-  ItemSize(bb, style.FramePadding.y);
-  if (!ItemAdd(bb, id))
-    return false;
-
-  // Render
-  window->DrawList->PathClear();
-
-  int num_segments = 30;
-  int start = abs(ImSin(g.Time * 1.8f) * (num_segments - 5));
-
-  const float a_min = IM_PI * 2.0f * ((float)start) / (float)num_segments;
-  const float a_max =
-      IM_PI * 2.0f * ((float)num_segments - 3) / (float)num_segments;
-
-  const ImVec2 centre =
-      ImVec2(pos.x + radius, pos.y + radius + style.FramePadding.y);
-
-  for (int i = 0; i < num_segments; i++) {
-    const float a = a_min + ((float)i / (float)num_segments) * (a_max - a_min);
-    window->DrawList->PathLineTo(
-        ImVec2(centre.x + ImCos(a + g.Time * 8) * radius,
-               centre.y + ImSin(a + g.Time * 8) * radius));
+    window->DrawList->AddCircleFilled(
+        ImVec2(pos.x + circleEnd - o1, bb.Min.y + r), r, bg_col);
+    window->DrawList->AddCircleFilled(
+        ImVec2(pos.x + circleEnd - o2, bb.Min.y + r), r, bg_col);
+    window->DrawList->AddCircleFilled(
+        ImVec2(pos.x + circleEnd - o3, bb.Min.y + r), r, bg_col);
   }
 
-  window->DrawList->PathStroke(color, false, thickness);
-}
+  bool Spinner(const char *label, float radius, int thickness,
+               const ImU32 &color)
+  {
+    ImGuiWindow *window = GetCurrentWindow();
+    if (window->SkipItems)
+      return false;
+
+    ImGuiContext &g = *GImGui;
+    const ImGuiStyle &style = g.Style;
+    const ImGuiID id = window->GetID(label);
+
+    ImVec2 pos = window->DC.CursorPos;
+    ImVec2 size((radius)*2, (radius + style.FramePadding.y) * 2);
+
+    const ImRect bb(pos, ImVec2(pos.x + size.x, pos.y + size.y));
+    ItemSize(bb, style.FramePadding.y);
+    if (!ItemAdd(bb, id))
+      return false;
+
+    // Render
+    window->DrawList->PathClear();
+
+    int num_segments = 30;
+    int start = abs(ImSin(g.Time * 1.8f) * (num_segments - 5));
+
+    const float a_min = IM_PI * 2.0f * ((float)start) / (float)num_segments;
+    const float a_max =
+        IM_PI * 2.0f * ((float)num_segments - 3) / (float)num_segments;
+
+    const ImVec2 centre =
+        ImVec2(pos.x + radius, pos.y + radius + style.FramePadding.y);
+
+    for (int i = 0; i < num_segments; i++)
+    {
+      const float a = a_min + ((float)i / (float)num_segments) * (a_max - a_min);
+      window->DrawList->PathLineTo(
+          ImVec2(centre.x + ImCos(a + g.Time * 8) * radius,
+                 centre.y + ImSin(a + g.Time * 8) * radius));
+    }
+
+    window->DrawList->PathStroke(color, false, thickness);
+  }
 
 } // namespace ImGui
 
@@ -168,9 +178,11 @@ float deltatime = 0.f;
 
 bool vsy = true;
 
-std::string FrameRate() {
+std::string FrameRate()
+{
   frames++;
-  if ((frameclock.GetAsMs() / 1000) >= 1.f) {
+  if ((frameclock.GetAsMs() / 1000) >= 1.f)
+  {
     fps = frames;
     frames = 0;
     frameclock.Reset();
@@ -182,7 +194,8 @@ bool initps = true;
 
 bool task = true;
 
-bool isev(int num) {
+bool isev(int num)
+{
   if (num % 2 == 0)
     return true;
   else
@@ -191,9 +204,11 @@ bool isev(int num) {
   return false;
 }
 
-unsigned hash_str(const char *s) {
+unsigned hash_str(const char *s)
+{
   unsigned h = 37;
-  while (*s) {
+  while (*s)
+  {
     h = (h * 54059) ^ (s[0] * 76963);
     s++;
   }
@@ -202,10 +217,12 @@ unsigned hash_str(const char *s) {
 
 Base::Timer ztm;
 
-bool task1(std::string msg) {
+bool task1(std::string msg)
+{
   task_ = "Init Loader!";
 
-  while (ztm.GetAsMs() >= 50000000) {
+  while (ztm.GetAsMs() >= 50000000)
+  {
     task_ = "Init: " + std::to_string(ztm.GetAsMs()) + "/" +
             std::to_string(50000 * 1000);
   }
@@ -215,7 +232,8 @@ bool task1(std::string msg) {
 
 bool prevcrash = false;
 
-int main(void) {
+int main(void)
+{
   stolenc = std::make_unique<Base::StealConsole>();
   NImGui::App app("Craftus-Next", NImGui::Vec2i(900, 400),
                   NImGui::BORDERLESS | NImGui::TRANSPARENT);
@@ -238,7 +256,8 @@ int main(void) {
   ztm.Reset();
   // std::future<bool> ftt = std::async(task1, "NULL");
   // task = false;
-  while (app.IsRunning()) {
+  while (app.IsRunning())
+  {
     task_ = std::to_string(ztm.GetAsMs()) + "/" + std::to_string(5000);
     ImGui::Begin("Test", NULL,
                  ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoBackground |
@@ -263,7 +282,8 @@ int main(void) {
     ImGui::End();
     prevcrash = false;
     app.SwapBuffers();
-    if (!task || ztm.GetAsMs() > 5000) {
+    if (!task || ztm.GetAsMs() > 5000)
+    {
       initps = false;
 
       // t1.join();
@@ -290,25 +310,28 @@ int main(void) {
   vao_->LD7();
   vao_->Create(&vtx, LLC_ARRAYSIZE(vtx), sizeof(Base::WorldVertex));
   vao_->AddAttrInfo(0, 3, 0, false, sizeof(Base::WorldVertex),
-                   (void *)offsetof(Base::WorldVertex, position));
+                    (void *)offsetof(Base::WorldVertex, position));
   vao_->AddAttrInfo(1, 2, 0, false, sizeof(Base::WorldVertex),
-                   (void *)offsetof(Base::WorldVertex, texcoords));
+                    (void *)offsetof(Base::WorldVertex, texcoords));
   vao_->AddAttrInfo(2, 3, 0, false, sizeof(Base::WorldVertex),
-                   (void *)offsetof(Base::WorldVertex, color));
+                    (void *)offsetof(Base::WorldVertex, color));
   vao_->UnBind();
   Base::Renderer *llc_renderer = 0;
   llc_renderer->LD7();
   llc_renderer->Init(app.GetMonitorSize().x, app.GetMonitorSize().y);
   color_t llc_quad((uint8_t)255, 255, 255, 255);
-  std::cout << CalcPMT(4-1) << std::endl;
-  while (app.IsRunning()) {
+  std::cout << CalcPMT(4 - 1) << std::endl;
+  while (app.IsRunning())
+  {
     llc_renderer->BeginDraw();
-    if (prevcrash) {
+    if (prevcrash)
+    {
       ImGui::End();
       prevcrash = false;
     }
 
-    if (!updt) {
+    if (!updt)
+    {
       app.SetWindowPos(NImGui::Vec2i(
           (app.GetMonitorSize().x / 2) - (app.GetWindowSize().x / 2),
           (app.GetMonitorSize().y / 2) - (app.GetWindowSize().y / 2)));
